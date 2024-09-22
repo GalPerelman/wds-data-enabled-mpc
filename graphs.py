4cc440efd09956f373a781cb01082df4efb19745
GalPerelman <peligur@gmail.com>
2024-09-22 12:56:21 +0300

    update paper customized graphs

--- graphs.py
+++ graphs.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def customize_plot_fossolo(experiment):
@@ -9,7 +10,7 @@ def customize_plot_fossolo(experiment):
         s["plot"] = True
     experiment.plot_legend_cols = 5
     fig = experiment.plot(experiment.wds)
-    fig = experiment.plot_comparison_signals(fig, signals=experiment.compare_signals)
+    fig, u, y = experiment.plot_comparison_signals(fig, signals=experiment.compare_signals)
     axes = fig.axes
     axes[0].set_ylim(6, 44)
     axes[1].set_ylim(26, 65)
@@ -17,6 +18,16 @@ def customize_plot_fossolo(experiment):
     axes[-1].set_xlim(experiment.n_train, experiment.n_train + experiment.experiment_horizon + 4)
     fig.set_size_inches(8, 6)
     plt.subplots_adjust(left=0.125, right=0.9, bottom=0.1, top=0.9)
+
+    mae, me = experiment.get_error(y=y)
+    ref = np.ones(y.shape) * experiment.y_ref
+    cost = np.linalg.norm(y - ref, 'fro') ** 2 + experiment.input_loss * np.linalg.norm(u, 'fro') ** 2
+    v_count, v_rate = experiment.get_violations(y=y)
+    print(f"{s['name']} --> Cost: {cost:.3f} | MAE: {mae:.3f} | ME: {me:.3f}")
+    print(f"{s['name']} --> {experiment.y_lb}-{experiment.y_ub} Violations: {v_count:.0f}"
+          f" | Violations Rate: {v_rate:.3f}")
+    v_count, v_rate = experiment.get_violations(y=y, lb=20, ub=40)
+    print(f"{s['name']} --> {20}-{40} Violations: {v_count:.0f} | Violations Rate: {v_rate:.3f}")
 
 
 def customize_plot_pescara(experiment):
